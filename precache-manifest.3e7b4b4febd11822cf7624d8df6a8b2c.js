c230763127be81e0f4ccc8920fcd61e2ce28f077
Majevadiya Meet <you@example.com>
2020-09-13 19:55:22 +0530

    Updates

---
+++ precache-manifest.3e7b4b4febd11822cf7624d8df6a8b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd9b064a69c8fca02db6f255f498beb",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "f7af40e91c41e622c6b5",
+    "url": "/Shopping-Cart/static/css/2.c31a3e37.chunk.css"
+  },
+  {
+    "revision": "8904c19145fc86411e25",
+    "url": "/Shopping-Cart/static/css/main.bd71f3b0.chunk.css"
+  },
+  {
+    "revision": "f7af40e91c41e622c6b5",
+    "url": "/Shopping-Cart/static/js/2.95f27975.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Shopping-Cart/static/js/2.95f27975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8904c19145fc86411e25",
+    "url": "/Shopping-Cart/static/js/main.606abcfe.chunk.js"
+  },
+  {
+    "revision": "f1e1f4e2f1aee800ac6a",
+    "url": "/Shopping-Cart/static/js/runtime-main.e78ba9bc.js"
+  }
+]);
